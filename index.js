7094d63a27ee536ab4dd594bf9ecf3823dedd83c
Ashish2211 <42649141+Ashish2211@users.noreply.github.com>
2019-06-30 22:02:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function showCreatePostModal(){
+    var modal = document.getElementById("create-post-modal");
+    modal.style.display = "flex";
+  }
+
+  function hideCreatePostModal(){
+    var modal = document.getElementById("create-post-modal");
+    modal.style.display = "none";
+  }
+
+  function openBlogList(){
+    location.href = "file:///C:/Users/ASHISH%20KUMAR/Desktop/Scribb%20updated/ScribblerForUpgrad-HomePage/html/bloglist.html"
+  }
